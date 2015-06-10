37b5fec1dbca5071eb9718815bca984f335a7398
masterismet <artucismet@gmail.com>
2015-06-10 16:52:02 +0300

    13.1

--- internshipmainapplication/internshipmainapplication/app2/angcode.js
+++ internshipmainapplication/internshipmainapplication/app2/angcode.js
@@ -28,8 +28,53 @@ myModule.directive('uiSelectable', function ($parse) {
 
 myModule.controller('TempController', ['$scope', '$rootScope', '$timeout', function ($scope, $rootScope, $timeout) {   
 
-  //  $scope.documents = [1, 2, 3, 4, 5, 6];
+ var mounthlenghts = [
+               { "name": "jan", "day": 31 },
+               { "name": "feb", "day": 28 },
+               { "name": "Mar", "day": 31 },
+               { "name": "Apr", "day": 30 },
+               { "name": "may", "day": 31 },
+               { "name": "jun", "day": 30 },
+               { "name": "Jul", "day": 31 },
+               { "name": "Aug", "day": 31 },
+               { "name": "Seb", "day": 30 },
+               { "name": "Oct", "day": 31 },
+               { "name": "Nov", "day": 30 },
+               { "name": "Dec", "day": 31 }];      
+  
+ var arraya = new Array();
+    arraya = [];
+ var arrayb = new Array();
+ arrayb = [];
+ var arrayc = new Array();
+ arrayc = [];
+ var object = [];
+   // var documents= new 
+ var i, j=10;
+ while (j < 12) {
+     for (i = 0; i <mounthlenghts[j].day; i++) {
+         var k = 0;
+         while (k < 24) {
+             object = {
+                  hour: i,
+                   day: k,
+                "name": mounthlenghts[j].name,
+             }
+             arraya.push(object);
 
+
+             k++;
+         }       
+
+     }
+
+     j++;
+
+ }
+   // $scope.documents = arrayc.concat(arraya, arrayb);
+    //console.log(arraya);  
+
+    /// other operations
     $scope.selectedItems = [];
     $scope.$watch('selectedItems', function (data) {
 
@@ -49,37 +94,33 @@ myModule.controller('TempController', ['$scope', '$rootScope', '$timeout', funct
     }
 
 }]);
-myModule.directive("dayMounths", function ($parse) {
-    var mounthdays =[
-               { "name": "jan", "day": 31 },
-               { "name": "feb", "day": 28 },
-               { "name": "Mar", "day": 31 },
-               { "name": "Apr", "day": 30 },
-               { "name": "may", "day": 31 },
-               { "name": "jun", "day": 30 },
-               { "name": "Jul", "day": 31 },
-               { "name": "Aug", "day": 31 },
-               { "name": "Seb", "day": 30 },
-               { "name": "Oct", "day": 31 },
-               { "name": "Nov", "day": 30 },
-               { "name": "Dec", "day": 31 }];
+
+
+
+myModule.directive("dayMounths", function ($scope, $interpolate) {
+
     return function (scope, element, attrs) {
-        var i, j = 0, dokument=0;
+        var i, j = 0, document=0;
         console.log("diziyi okudu");
-        while (j < 12) {
+       // while (j < 12) {
             //for (i = 0; i < mounthdays[j].day; i++) {
                 var k = 0;
                                 
             // while (k < 24) {
 
-                    var content = ' <br><span>{{'+document+'}}</span><span ng-click="dosomething($index)">Clicke me</span><br>';
+                    /*var content = '<span> {{document}} </span><span ng-click="dosomething($index)"></span>';
                     var listElem = angular.element(content);
-                    var compileFn = $parse(listElem);
+                    var compileFn = $interpolate(listElem);
                     compileFn(scope);
-                    element.append(listElem);
+                    element.append(listElem);*/
+                    var html = '<span> {{document}} </span><span ng-click="dosomething($index)"></span>';
+                    $scope.interpolate = $interpolate(html)($scope);
+                    
+
+                    
 
                     k++;
-                    dokument++;
+                    document++;
 
                // }
                 console.log(j);
@@ -88,7 +129,7 @@ myModule.directive("dayMounths", function ($parse) {
 
             j = j + 1;
 
-        }
+     //   }
         
     } // end of return
 
