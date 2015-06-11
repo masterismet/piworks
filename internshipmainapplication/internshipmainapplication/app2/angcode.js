32cfb40ddc932adf73f357f489e986b13491729f
masterismet <artucismet@gmail.com>
2015-06-11 09:33:03 +0300

    14.0

--- internshipmainapplication/internshipmainapplication/app2/angcode.js
+++ internshipmainapplication/internshipmainapplication/app2/angcode.js
@@ -28,52 +28,6 @@ myModule.directive('uiSelectable', function ($parse) {
 
 myModule.controller('TempController', ['$scope', '$rootScope', '$timeout', function ($scope, $rootScope, $timeout) {   
 
- var mounthlenghts = [
-               { "name": "jan", "day": 31 },
-               { "name": "feb", "day": 28 },
-               { "name": "Mar", "day": 31 },
-               { "name": "Apr", "day": 30 },
-               { "name": "may", "day": 31 },
-               { "name": "jun", "day": 30 },
-               { "name": "Jul", "day": 31 },
-               { "name": "Aug", "day": 31 },
-               { "name": "Seb", "day": 30 },
-               { "name": "Oct", "day": 31 },
-               { "name": "Nov", "day": 30 },
-               { "name": "Dec", "day": 31 }];      
-  
- var arraya = new Array();
-    arraya = [];
- var arrayb = new Array();
- arrayb = [];
- var arrayc = new Array();
- arrayc = [];
- var object = [];
-   // var documents= new 
- var i, j=10;
- while (j < 12) {
-     for (i = 0; i <mounthlenghts[j].day; i++) {
-         var k = 0;
-         while (k < 24) {
-             object = {
-                  hour: i,
-                   day: k,
-                "name": mounthlenghts[j].name,
-             }
-             arraya.push(object);
-
-
-             k++;
-         }       
-
-     }
-
-     j++;
-
- }
-   // $scope.documents = arrayc.concat(arraya, arrayb);
-    //console.log(arraya);  
-
     /// other operations
     $scope.selectedItems = [];
     $scope.$watch('selectedItems', function (data) {
@@ -94,47 +48,63 @@ myModule.controller('TempController', ['$scope', '$rootScope', '$timeout', funct
     }
 
 }]);
+myModule.controller("dayCalculateController", function ($scope) {
+    var mounthlenghts = [
+               { "name": "jan", "day": 31 },
+               { "name": "feb", "day": 28 },
+               { "name": "Mar", "day": 31 },
+               { "name": "Apr", "day": 30 },
+               { "name": "may", "day": 31 },
+               { "name": "jun", "day": 30 },
+               { "name": "Jul", "day": 31 },
+               { "name": "Aug", "day": 31 },
+               { "name": "Seb", "day": 30 },
+               { "name": "Oct", "day": 31 },
+               { "name": "Nov", "day": 30 },
+               { "name": "Dec", "day": 31 }];
 
+    var arraya = new Array();
+    arraya = [];
+    var arrayb = new Array();
+    arrayb = [];
+    var arrayc = new Array();
+    arrayc = [];
+    var object = [];
+    // var documents= new 
+    var i, j = 10;
+    while (j < 12) {
+        for (i = 0; i < mounthlenghts[j].day; i++) {
+            var k = 0;
+            while (k < 24) {
+                object = {
+                    hour: i,
+                    day: k,
+                    "name": mounthlenghts[j].name,
+                }
+                arraya.push(object);
+                k++;
+            }
 
+        }
 
-myModule.directive("dayMounths", function ($scope, $interpolate) {
+        j++;
 
-    return function (scope, element, attrs) {
-        var i, j = 0, document=0;
-        console.log("diziyi okudu");
-       // while (j < 12) {
-            //for (i = 0; i < mounthdays[j].day; i++) {
-                var k = 0;
-                                
-            // while (k < 24) {
-
-                    /*var content = '<span> {{document}} </span><span ng-click="dosomething($index)"></span>';
-                    var listElem = angular.element(content);
-                    var compileFn = $interpolate(listElem);
-                    compileFn(scope);
-                    element.append(listElem);*/
-                    var html = '<span> {{document}} </span><span ng-click="dosomething($index)"></span>';
-                    $scope.interpolate = $interpolate(html)($scope);
-                    
-
-                    
-
-                    k++;
-                    document++;
-
-               // }
-                console.log(j);
-                
-         //   }
-
-            j = j + 1;
-
-     //   }
-        
-    } // end of return
-
-
-
-    //
+    }
+    $scope.documents = arrayc.concat(arraya, arrayb);
+    console.log(arraya.day);
 
 });
+
+  myModule.directive("evalExpression", function ($compile) {    
+     
+           return function (scope, element, attrs) {
+            //   var content = "<ul><li ng-repeat='city in documents '>{{city}}</li></ul>"
+            var content = '<div class="parent" ng-repeat="document in documents"><span>{{document}}</span><span ng-click="dosomething($index)">Clicke me</span></div>';
+            var listElem = angular.element(content);
+            var compileFn = $compile(listElem);
+            compileFn(scope);
+            element.append(listElem);
+        }
+      
+  });
+ 
