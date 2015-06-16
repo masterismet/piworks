66811b49d4ea1b9ca911aac75f5c53e76eb2947a
masterismet <artucismet@gmail.com>
2015-06-16 11:55:01 +0300

    17.1

--- internshipmainapplication/internshipmainapplication/app2/angcode.js
+++ internshipmainapplication/internshipmainapplication/app2/angcode.js
@@ -1,5 +1,4 @@
-﻿// Code goes here
-var myModule = angular.module('myApp', []);
+﻿var myModule = angular.module('myApp', []);
 
 myModule.directive('uiSelectable', function ($parse) {
     return {
@@ -25,33 +24,10 @@ myModule.directive('uiSelectable', function ($parse) {
         }
     }
 });
-myModule.directive('uiSelectable2', function ($parse) {
-    return {
-        link: function (scope, element, attrs, ctrl) {
-
-            scope.$on('clearselection', function (event, hour) {
-                element.find('.ui-selected').removeClass('ui-selected')
-            });
-            console.log("burası tammam");
-
-            element.selectable({
-                stop: function (evt, ui) {
-                    var collection = scope.$eval(attrs.docArray)
-                    var selected = element.find('div.parent.ui-selected').map(function () {
-                        var idx = $(this).index();
-                        return { document: collection[idx] }
-                    }).get();
-
-                    scope.selectedItems = selected;
-                    scope.$apply()
-                }
-            });
-        }
-    }
-});
 
 
-myModule.controller('TempController', ['$scope', '$rootScope', '$timeout', function ($scope, $rootScope, $timeout) {   
+
+myModule.controller('TempController', ['$scope', '$rootScope', '$timeout', function ($scope, $rootScope, $timeout) {
 
     /// other operations
     $scope.selectedItems = [];
@@ -67,13 +43,15 @@ myModule.controller('TempController', ['$scope', '$rootScope', '$timeout', funct
         }, 100);
     }
 
-    $scope.dosomething = function (index) {
-        console.log('You clicked ' + index);
-
+    $scope.setHour = function (document) {
+        var update = document.hour;
+        update.push($scope.hour);
+        console.log(update);
     }
-
+   
+   
 }]);
-myModule.controller("dayCalculateController", function ($scope,$parse) {
+myModule.controller("dayCalculateController", function ($scope, $parse) {
     var mounthlenghts = [
                { "name": "jan", "day": 31 },
                { "name": "feb", "day": 28 },
@@ -87,7 +65,7 @@ myModule.controller("dayCalculateController", function ($scope,$parse) {
                { "name": "Oct", "day": 31 },
                { "name": "Nov", "day": 30 },
                { "name": "Dec", "day": 31 }];
-    $scope.hours = ["0", "1", "2", "3", "4"];//, "5", "6", "7", "8", "9", "10","11","12","13","14","15","16","17","18","19","20","21","22","23"];
+    $scope.hours = [0, 1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
 
     var arraya = new Array();
     arraya = [];
@@ -95,84 +73,26 @@ myModule.controller("dayCalculateController", function ($scope,$parse) {
     arrayb = [];
     var arrayc = new Array();
     arrayc = [];
-    var object = [];
-    // var documents= new 
-    var i, j =10;
-    while (j < 12) {
-        for (i = 0; i < mounthlenghts[j].day; i++) {
-            var k = 20;
-           // while (k < 24) {
-                object = {
-                   // hour: k,
-                    day: i,
-                    "name": mounthlenghts[j].name,
-                }
-                arraya.push(object);
-                $scope.document = $parse('object');
-             //  k++;
-           }
+    var time = [];
+    var day, mounth = 10;
+    while (mounth < 12) {
+        for (day = 1; day <= mounthlenghts[mounth].day; day++) {
+    time = {
+                hour: [],
+                day: day,
+                "mounth": mounthlenghts[mounth].name,
+            }
+             arraya.push(time);
+           //  $scope.documents.push(time);
+           // $scope.document = $parse('time');
 
-       // }
-
-        j++;
-
-    }
-    $scope.documents = arrayc.concat(arraya, arrayb);
-   
-    
-    // here is so important plss give more attention , good luck
-});
-
-  /*myModule.directive("evalExpression", function ($parse) {    
-      var mounthlenghts = [
-                { "name": "jan", "day": 31 },
-                { "name": "feb", "day": 28 },
-                { "name": "Mar", "day": 31 },
-                { "name": "Apr", "day": 30 },
-                { "name": "may", "day": 31 },
-                { "name": "jun", "day": 30 },
-                { "name": "Jul", "day": 31 },
-                { "name": "Aug", "day": 31 },
-                { "name": "Seb", "day": 30 },
-                { "name": "Oct", "day": 31 },
-                { "name": "Nov", "day": 30 },
-                { "name": "Dec", "day": 31 }];
-
-      var content = '';
-      var documentt = [];
-      var temp;
-      var object = [];
-      // var documents= new 
-      var i, j = 10;
-      while (j < 12) {
-          for (i = 0; i < mounthlenghts[j].day; i++) {
-              var k = 20;
-              while (k < 24) {
-                  documentt = {
-                      hour: i,
-                      day: k,
-                      "name": mounthlenghts[j].name,
-                  }
-                  //arraya.push(object);
-                  $scope.document = $parse('documentt');                  
-                  k++;
-              }
-
-          }
-
-          j++;
-
-      }
-      /*$scope.name = 'Manish';
-$interpolate(string)($scope);
-      /*
-      return function (scope, element, attrs) {
-            var listElem = angular.element(content);
-            var compileFn = $compile(listElem);
-            compileFn(scope);
-            element.append(listElem);
         }
 
-        });
-      */
-  
+       mounth++;
+
+    }
+   $scope.documents = arrayc.concat(arraya, arrayb);
+
+
+    // here is so important plss give more attention , good luck
+});
