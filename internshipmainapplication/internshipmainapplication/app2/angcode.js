975a76e88c9703fc1752aa7a68e8ecf3a6fbe2de
masterismet <artucismet@gmail.com>
2015-06-17 08:58:42 +0300

    18.0

--- internshipmainapplication/internshipmainapplication/app2/angcode.js
+++ internshipmainapplication/internshipmainapplication/app2/angcode.js
@@ -12,7 +12,7 @@ myModule.directive('uiSelectable', function ($parse) {
             element.selectable({
                 stop: function (evt, ui) {
                     var collection = scope.$eval(attrs.docArray)
-                    var selected = element.find('div.parent.ui-selected');
+                    var selected = element.find('div.selection hide.ui-selected');
                     console.log(selected);
                        selected.map(function () {
                         var idx = $(this).index();
@@ -73,7 +73,7 @@ myModule.controller("dayCalculateController", function ($scope, $parse) {
     var arrayc = new Array();
     arrayc = [];
     var time = [];
-    var day, mounth = 10;
+    var day, mounth = 0;
     while (mounth < 12) {
         for (day = 1; day <= mounthlenghts[mounth].day; day++) {
     time = {               
@@ -92,3 +92,84 @@ myModule.controller("dayCalculateController", function ($scope, $parse) {
 
     // here is so important plss give more attention , good luck
 });
+ $(window).load(function () {
+            var SECTION_DIV = "section > div";
+            var $SELECTION = $("div.selection");
+            var $selected = $();
+            var point = [0, 0];
+            $(SECTION_DIV).on("mousedown touchstart", function (e) {
+                if (e.which === 1 || e.type == "touchstart") {
+                    if (!e.ctrlKey) {
+                        $(SECTION_DIV).removeClass("marked mark");
+                    }
+                    $(this).addClass("marked mark");
+                }
+            });
+            $("body").on("mousedown touchstart", function (i) {
+                if (i.type == "touchstart" || (i.which === 1 && !$(i.target).is(SECTION_DIV))) {
+                    if (!i.ctrlKey && !(i.type == "touchstart" && $(i.target).is(SECTION_DIV))) {
+                        $(SECTION_DIV).removeClass("marked mark");
+                    } else {
+                        console.log(i.target);
+                    }
+                    if (i.type == "touchstart") {
+                        i.pageX = i.originalEvent.touches[0].pageX;
+                        i.pageY = i.originalEvent.touches[0].pageY;
+                    }
+                    if ((point[0] != i.pageX && point[1] != i.pageY) || i.type == "mousedown") {
+                        point[0] = i.pageX;
+                        point[1] = i.pageY;
+                        $SELECTION.css({
+                            "top": i.pageY,
+                            "left": i.pageX
+                        });
+                        $SELECTION.removeClass("hide");
+                    }
+                    $(this).on("mousemove touchmove", function (f) {
+
+                        if (f.type == "touchmove") {
+                            f.pageX = f.originalEvent.touches[0].pageX;
+                            f.pageY = f.originalEvent.touches[0].pageY;
+                        }
+                        var rect = {
+                            x: i.pageX > f.pageX ? f.pageX : i.pageX,
+                            y: i.pageY > f.pageY ? f.pageY : i.pageY,
+                            w: i.pageX > f.pageX ? i.pageX - f.pageX : f.pageX - i.pageX,
+                            h: i.pageY > f.pageY ? i.pageY - f.pageY : f.pageY - i.pageY
+                        };
+                        $SELECTION.css({
+                            "top": rect.y - 1,
+                            "left": rect.x - 1,
+                            "width": rect.w + 1,
+                            "height": rect.h + 1
+                        });
+                        var $marked;
+                        var filterUnmark = function (index) {
+                            if ($(this).is(".marked") && f.ctrlKey) {
+                                return (false);
+                            } else {
+                                return (true);
+                            }
+                        }
+                        // ------------------------------------
+                        $selected = $(SECTION_DIV).overlap({
+                            rect: rect,
+                            mark: "mark",
+                            filterUnmark: filterUnmark,
+                            unmark: true
+                        });
+                        //-------------------------------------
+                    });
+                }
+                i.preventDefault();
+            }).on("mouseup touchend touchcancel", function (e) {
+                if (this === e.target || $SELECTION[0] === e.target || e.type != "mouseup") {
+                    if (e.which === 1 || e.type != "mouseup") {
+                        $(this).off("mousemove touchmove");
+                        $SELECTION.addClass("hide").removeAttr("style");
+                        $selected.addClass("marked");
+                        e.preventDefault();
+                    }
+                }
+            });
+        });
