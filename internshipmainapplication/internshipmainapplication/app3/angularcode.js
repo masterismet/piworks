8018cf8d009f7492e2b121259cbdadd2004f43d7
masterismet <artucismet@gmail.com>
2015-06-19 09:59:01 +0300

    201

--- internshipmainapplication/internshipmainapplication/app3/angularcode.js
+++ internshipmainapplication/internshipmainapplication/app3/angularcode.js
@@ -1,6 +1,6 @@
 ﻿var myModule = angular.module('myApp', []);
 myModule.controller("dayCalculateController", function () {});
-myModule.directive("evalExpression", function ($compile) {
+myModule.directive("evalExpression", function ($compile,$rootScope) {
     var mounthlenghts = [
               { "name": "jan", "day": 31 },
               { "name": "feb", "day": 28 },
@@ -14,14 +14,35 @@ myModule.directive("evalExpression", function ($compile) {
               { "name": "Oct", "day": 31 },
               { "name": "Nov", "day": 30 },
               { "name": "Dec", "day": 31 }];
-    var content = '';//'<select multiple="multiple" name="duallistbox_demo1[]">';
+    var content = '';
     content = content + '<select multiple="multiple" name="duallistbox_demo1[]">';
-    var day, mounth = 0, hour = 0, x = 2;
+    var day, mounth = 0, hour = 0, x = 0;
+    var arraya = new Array();
+    arraya = [];
+    var arrayb = new Array();
+    arrayb = [];
+    var arrayc = new Array();
+    arrayc = [];
+    var time = [];
+    var documents = [];
     for (mounth = 0; mounth < 12; mounth++)
     {
         for (day = 1; day <= mounthlenghts[mounth].day; day++)
         {
-            if (day % 2 == 0) {                
+            for (hour = 0; hour < 24; hour++) {
+                time = {
+                    hour: hour,
+                    day: day,
+                    "mounth": mounthlenghts[mounth].name,
+                }
+                arraya.push(time);
+                
+                //documents.push(time);
+
+            }
+            
+          
+           /* if (day % 2 == 0) {                
                 for (hour = 0; hour < 24; hour++)
                 {                    
                     content = content + '<option value="date:' + hour + ',' + day + ',' + mounth + '"class="leftoptbox" >' + hour + '</option>';// class="leftoptbox"
@@ -38,13 +59,14 @@ myModule.directive("evalExpression", function ($compile) {
                 console.log("right");
 
             }
-
+            */
         }
+        x++;
    
-}
-
-
-        
+    }
+    $rootScope.columnBreak = x;
+    $rootScope.documents = arrayc.concat(arraya, arrayb);
+    content = content + '<option ng-repeat="document in documents " value="{{document}}" class="parent"  ng-class="{'+'new-col'+': startNewCol($index, columnBreak) }"> {{document.hour}}</option>';
             
         
     content = content + '</select>';
